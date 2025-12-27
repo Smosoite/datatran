f3bbb7a3fd18ce275825563e11dc8bb5a1d636bd
Smosoite <smurffiosoite@proton.me>
2025-12-28 00:15:17 +0200

    Updated find.tsx

--- app/find.tsx
+++ app/find.tsx
@@ -47,7 +47,7 @@ export default function FindScreen() {
         try {
             const hasSeen = await AsyncStorage.getItem('HAS_SEEN_FIND_TOUR');
             if (!hasSeen) {
-                setTimeout(() => startTour(), 500);
+                setTimeout(() => startTour(), 1500);
                 await AsyncStorage.setItem('HAS_SEEN_FIND_TOUR', 'true');
             }
         } catch (e) {
