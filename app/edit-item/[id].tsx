28b82b23490b15264539f14cab3039b75daf7c65
Smosoite <smurffiosoite@proton.me>
2026-01-10 02:49:33 +0200

    Updated [id].tsx

--- app/edit-item/[id].tsx
+++ app/edit-item/[id].tsx
@@ -1,5 +1,5 @@
-import '../i18n';
-import i18n from '../i18n';
+import '../../i18n';
+import i18n from '../../i18n';
 import { I18nextProvider } from 'react-i18next';
 import { useTranslation } from 'react-i18next';
 import React, { useState, useEffect } from 'react';
