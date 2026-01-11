2180dbb4d0c6a300e540c7780d8a0f2236487074
Smosoite <smurffiosoite@proton.me>
2026-01-11 20:19:09 +0200

    Updated index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -81,7 +81,7 @@ export default function HomeScreen() {
               numberOfLines={1}
               adjustsFontSizeToFit
               minimumFontScale={0.5}
-              style={[typography.button, typography.shadow, styles.buttonText, { color: colors.subtext, textShadowColor: colors.textShadow }]}
+              style={[typography.button, styles.buttonText, { color: colors.subtext, textShadowColor: colors.textShadow }]}
             >
               {t('dashboard.scanItem')}
             </Text>
