189b786125e6c4cfa5e90098079d8b7f4f3eef11
Smosoite <smurffiosoite@proton.me>
2026-01-11 22:17:36 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ export default function TabLayout() {
   return (
     <Tabs
         screenOptions={{
-        tabBarActiveTintColor: colors.primary,
+        tabBarActiveTintColor: colors.selector,
         tabBarStyle: {
           backgroundColor: colors.card,
           borderTopColor: colors.border,
