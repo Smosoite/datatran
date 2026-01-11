d98093dfb0acd6b9ffa6741b6331b70e91da82fe
Smosoite <smurffiosoite@proton.me>
2026-01-11 22:13:02 +0200

    Updated [id].tsx

--- app/edit-item/[id].tsx
+++ app/edit-item/[id].tsx
@@ -413,7 +413,7 @@ export default function EditItemScreen() {
         {updating ? (
           <ActivityIndicator color={colors.text || '#fff'} />
         ) : (
-          <Text style={[typography.button, styles.buttonText, { color: '#fff' }]}>{t('general.save')}</Text>
+          <Text style={[typography.button, styles.buttonText, { color: colors.selector }]}>{t('general.save')}</Text>
         )}
       </Pressable>
     </ScrollView>
