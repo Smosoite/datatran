f93ca1846dc3377497bab4d7b443d51daaf868b1
Smosoite <smurffiosoite@proton.me>
2026-01-12 02:33:18 +0200

    Updated ChangePasswordModal.tsx

--- components/ChangePasswordModal.tsx
+++ components/ChangePasswordModal.tsx
@@ -10,6 +10,7 @@ import { useTheme } from '../providers/ThemeProvider';
 export default function ChangePasswordModal({ isVisible, onClose, themeColors, styles }) {
   const { t } = useTranslation();
   const router = useRouter();
+  const { colors } = useTheme(); 
   
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
